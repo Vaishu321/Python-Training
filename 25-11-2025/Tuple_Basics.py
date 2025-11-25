af66c345fe2758157f5ea9e4051e49a6415b8235
G N Vaishnavi <138749951+Vaishu321@users.noreply.github.com>
2025-11-25 15:38:55 +0530

    Update Tuple_Basics.py

--- 25-11-2025/Tuple_Basics.py
+++ 25-11-2025/Tuple_Basics.py
@@ -1,4 +1,4 @@
-'''
+
 numbers=(10,20,30)
 print(numbers.count(10))
 names=("Abdullah","Daniel","David")
@@ -27,19 +27,4 @@ print(a,b,c)
 
 #nested tuples
 employee = ("John",23,("Bangalore","India"))
-print(employee[2][0])'''
-
-#Calculate the max and min of tuples without using max and min
-numbers=(33,20,30,60,50)
-
-def max_min(nums):
-    min_value = nums[0]
-    max_value = nums[0]
-
-    for i in range(1,len(nums)):
-        if nums[i] > max_value:
-            max_value = nums[i]
-        elif nums[i] < min_value:
-            min_value = nums[i]
-    return max_value,min_value
-print(max_min(numbers))
+print(employee[2][0])
