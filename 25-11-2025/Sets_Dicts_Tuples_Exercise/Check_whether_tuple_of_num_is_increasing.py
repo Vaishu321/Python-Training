08ab678118e3443615e2606be0729eaf1c65c138
G N Vaishnavi <138749951+Vaishu321@users.noreply.github.com>
2025-11-26 14:41:13 +0530

    Update Check_whether_tuple_of_num_is_increasing.py

--- 25-11-2025/Sets_Dicts_Tuples_Exercise/Check_whether_tuple_of_num_is_increasing.py
+++ 25-11-2025/Sets_Dicts_Tuples_Exercise/Check_whether_tuple_of_num_is_increasing.py
@@ -1,9 +1,12 @@
-num=[2,3,4,5,6,7]
+num=(2,3,4,5,6,7)
+list0=list(num)
+list1=list(num)
 new=[]
-for i in num:
-    if i+1>1:
-        new.append(i)
-if new==num:
+for i in range (len(list0)):
+    y = min(list1)
+    new.append(y)
+    list1.remove(y)
+if new==list0:
     print("Yes its increasing")
 else:
     print("No")
