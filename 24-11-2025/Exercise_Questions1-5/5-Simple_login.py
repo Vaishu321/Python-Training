eafa06284639567215405eda2ffadcc8491034ce
G N Vaishnavi <138749951+Vaishu321@users.noreply.github.com>
2025-12-04 23:58:57 +0530

    Add files via upload

---
+++ 24-11-2025/Exercise_Questions1-5/5-Simple_login.py
@@ -0,0 +1,7 @@
+Username=input("Enter your Username: ")
+Password=input("Enter your Password: ")
+
+if Username=="admin" and Password=="1234":
+    print("Login Successful!")
+else:
+    print("Incorrect username or password.")
