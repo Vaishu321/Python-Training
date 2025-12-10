df29f7bce6b5cc9dc7793ad54e736681c291fe42
G N Vaishnavi <138749951+Vaishu321@users.noreply.github.com>
2025-12-10 14:10:07 +0530

    Update and rename print_rows_gt_value.py to print_rows_marks>75_value.py

--- 28-11-2025/Second_set_exercises/file_handling/print_rows_marks>75_value.py
+++ 28-11-2025/Second_set_exercises/file_handling/print_rows_marks>75_value.py
@@ -3,5 +3,5 @@ import csv
 with open("textfile_new/students.csv","r") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        if int(row["marks"])>85:
+        if int(row["marks"])>75:
             print(f"Name : {row["name"]} Marks: {row["marks"]}")
